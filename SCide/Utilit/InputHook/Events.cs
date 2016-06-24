3cc8f37e4167d76444abba80f3adfe4c1858231c
mrkriv <virus_450@mail.ru>
2016-06-24 18:28:25 +0300

    Add InputHook class

---
+++ SCide/Utilit/InputHook/Events.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ASM.Utilit
+{
+    public partial class InputHook
+    {
+        public static event MouseEventHandler MouseMove
+        {
+            add
+            {
+                EnsureSubscribedToGlobalMouseEvents();
+                s_MouseMove += value;
+            }
+
+            remove
+            {
+                s_MouseMove -= value;
+                TryUnsubscribeFromGlobalMouseEvents();
+            }
+        }
+
+        public static event EventHandler<MouseEventExtArgs> MouseMoveExt
+        {
+            add
+            {
+                EnsureSubscribedToGlobalMouseEvents();
+                s_MouseMoveExt += value;
+            }
+
+            remove
+            {
+
+                s_MouseMoveExt -= value;
+                TryUnsubscribeFromGlobalMouseEvents();
+            }
+        }
+
+        public static event MouseEventHandler MouseClick
+        {
+            add
+            {
+                EnsureSubscribedToGlobalMouseEvents();
+                s_MouseClick += value;
+            }
+            remove
+            {
+                s_MouseClick -= value;
+                TryUnsubscribeFromGlobalMouseEvents();
+            }
+        }
+
+        public static event EventHandler<MouseEventExtArgs> MouseClickExt
+        {
+            add
+            {
+                EnsureSubscribedToGlobalMouseEvents();
+                s_MouseClickExt += value;
+            }
+            remove
+            {
+                s_MouseClickExt -= value;
+                TryUnsubscribeFromGlobalMouseEvents();
+            }
+        }
+
+        public static event MouseEventHandler MouseDown
+        {
+            add
+            {
+                EnsureSubscribedToGlobalMouseEvents();
+                s_MouseDown += value;
+            }
+            remove
+            {
+                s_MouseDown -= value;
+                TryUnsubscribeFromGlobalMouseEvents();
+            }
+        }
+
+        public static event MouseEventHandler MouseUp
+        {
+            add
+            {
+                EnsureSubscribedToGlobalMouseEvents();
+                s_MouseUp += value;
+            }
+            remove
+            {
+                s_MouseUp -= value;
+                TryUnsubscribeFromGlobalMouseEvents();
+            }
+        }
+
+        public static event MouseEventHandler MouseWheel
+        {
+            add
+            {
+                EnsureSubscribedToGlobalMouseEvents();
+                s_MouseWheel += value;
+            }
+            remove
+            {
+                s_MouseWheel -= value;
+                TryUnsubscribeFromGlobalMouseEvents();
+            }
+        }
+
+        public static event MouseEventHandler MouseDoubleClick
+        {
+            add
+            {
+                EnsureSubscribedToGlobalMouseEvents();
+                if (s_MouseDoubleClick == null)
+                {
+                    s_DoubleClickTimer = new Timer
+                    {
+                        Interval = GetDoubleClickTime(),
+                        Enabled = false
+                    };
+                    s_DoubleClickTimer.Tick += DoubleClickTimeElapsed;
+                    MouseUp += OnMouseUp;
+                }
+                s_MouseDoubleClick += value;
+            }
+            remove
+            {
+                if (s_MouseDoubleClick != null)
+                {
+                    s_MouseDoubleClick -= value;
+                    if (s_MouseDoubleClick == null)
+                    {
+                        MouseUp -= OnMouseUp;
+                        s_DoubleClickTimer.Tick -= DoubleClickTimeElapsed;
+                        s_DoubleClickTimer = null;
+                    }
+                }
+                TryUnsubscribeFromGlobalMouseEvents();
+            }
+        }
+
+        private static void DoubleClickTimeElapsed(object sender, EventArgs e)
+        {
+            s_DoubleClickTimer.Enabled = false;
+            s_PrevClickedButton = MouseButtons.None;
+        }
+
+        private static void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Clicks < 1)
+                return;
+            if (e.Button.Equals(s_PrevClickedButton))
+            {
+                if (s_MouseDoubleClick != null)
+                    s_MouseDoubleClick.Invoke(null, e);
+                s_DoubleClickTimer.Enabled = false;
+                s_PrevClickedButton = MouseButtons.None;
+            }
+            else
+            {
+                s_DoubleClickTimer.Enabled = true;
+                s_PrevClickedButton = e.Button;
+            }
+        }
+
+        public static event KeyPressEventHandler KeyPress
+        {
+            add
+            {
+                EnsureSubscribedToGlobalKeyboardEvents();
+                s_KeyPress += value;
+            }
+            remove
+            {
+                s_KeyPress -= value;
+                TryUnsubscribeFromGlobalKeyboardEvents();
+            }
+        }
+
+        public static event KeyEventHandler KeyUp
+        {
+            add
+            {
+                EnsureSubscribedToGlobalKeyboardEvents();
+                s_KeyUp += value;
+            }
+            remove
+            {
+                s_KeyUp -= value;
+                TryUnsubscribeFromGlobalKeyboardEvents();
+            }
+        }
+
+        public static event KeyEventHandler KeyDown
+        {
+            add
+            {
+                EnsureSubscribedToGlobalKeyboardEvents();
+                s_KeyDown += value;
+            }
+            remove
+            {
+                s_KeyDown -= value;
+                TryUnsubscribeFromGlobalKeyboardEvents();
+            }
+        }
+    }
+}
